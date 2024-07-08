cee4a173a7fb5a853f849c7b65cab63ff0c32907
quân <116118843+lagessiehcs@users.noreply.github.com>
2024-07-08 16:43:27 +0200

    parameterize NumStates and NumControls

--- Python/mpc_optimizer.py
+++ Python/mpc_optimizer.py
@@ -9,6 +9,8 @@ class MpcOptimizer:
         self.Controls = np.array(u)
 
         # Optional properties with defaults
+        self.VRef = options.get('VRef', 1)
+        self.OmegaRef = options.get('OmegaRef', 0)
         self.PosXLim = options.get('PosXLim', [-np.inf, np.inf])
         self.PosYLim = options.get('PosYLim', [-np.inf, np.inf])
         self.OrientLim = options.get('OrientLim', [-np.inf, np.inf])
@@ -23,6 +25,9 @@ class MpcOptimizer:
 
         self.Q = np.diag([weight_x, weight_y, weight_theta])
         self.R = np.diag([weight_v, weight_omega])
+
+        self.NumStates = 3
+        self.NumControls = 2
         
         self.Args = {}
         self.Solver = None
@@ -33,30 +38,31 @@ class MpcOptimizer:
         y = ca.SX.sym('y')
         theta = ca.SX.sym('theta')
         states = ca.vertcat(x, y, theta)
-        numStates = states.size1()
 
         v = ca.SX.sym('v')
         omega = ca.SX.sym('omega')
         controls = ca.vertcat(v, omega)
-        numControls = controls.size1()
 
         rhs = ca.vertcat(v * ca.cos(theta), v * ca.sin(theta), omega)
         f = ca.Function('f', [states, controls], [rhs])
 
         # Decision variables (controls)
-        U = ca.SX.sym('U', numControls, self.N)
+        U = ca.SX.sym('U', self.NumControls, self.N)
 
         # Parameters (initial state and reference trajectory)
-        P = ca.SX.sym('P', numStates + self.N * (numStates + numControls))
+        P = ca.SX.sym('P', self.NumStates + self.N * (self.NumStates + self.NumControls))
 
         # States over the optimization problem
-        X = ca.SX.sym('X', numStates, (self.N + 1))
+        X = ca.SX.sym('X', self.NumStates, (self.N + 1))
 
         # Make the decision variable one column vector
-        OPT_variables = ca.vertcat(ca.reshape(X, 3 * (self.N + 1), 1), ca.reshape(U, 2 * self.N, 1))
+        OPT_variables = ca.vertcat(
+            X.reshape((-1, 1)),   # Example: 3x11 ---> 33x1 where 3=states, 11=N+1
+            U.reshape((-1, 1))
+        )
 
         # Calculate cost function
-        cost = self._cost_function(X, U, P, numStates, numControls)
+        cost = self._cost_function(X, U, P)
 
         # Calculate nonlinear constraints
         g = self._nonlinear_constraints(f, X, U, P)
@@ -71,49 +77,48 @@ class MpcOptimizer:
         u0 = self.Controls
         xs = path
 
-        self.Args['p'] = np.zeros((3 + 5 * self.N,))
-        self.Args['p'][0:3] = x0
+        self.Args['p'] = np.zeros((self.NumStates + (self.NumStates+self.NumControls) * self.N,))
+        self.Args['p'][0:self.NumStates] = x0
 
-        for k in range(1, self.N):
+        for k in range(self.N):
             x_next, y_next = xs[:, 4]
             x_ref, y_ref = xs[:, 0]
             theta_ref = np.arctan2(y_next - y_ref, x_next - x_ref)
 
-            u_ref = 1
-            omega_ref = 0
-            self.Args['p'][5 * k - 2:5 * k + 1] = [x_ref, y_ref, theta_ref]
-            self.Args['p'][5 * k + 1:5 * k + 3] = [u_ref, omega_ref]
+            v_ref = self.VRef
+            omega_ref = self.OmegaRef
+            self.Args['p'][(self.NumStates+self.NumControls)*(k+1)-2 : (self.NumStates+self.NumControls)*(k+1)-2+self.NumStates] = [x_ref, y_ref, theta_ref]
+            self.Args['p'][(self.NumStates+self.NumControls)*(k+1)+1 : (self.NumStates+self.NumControls)*(k+1)+1+self.NumControls] = [v_ref, omega_ref]
 
         # Initial value of the optimization variables
-        self.Args['x0'] = np.concatenate([X0.ravel(), u0.ravel()])
+        self.Args['x0'] = ca.vertcat(
+            ca.reshape(X0, -1, 1),
+            ca.reshape(u0, -1, 1)
+        )
         sol = self.Solver(x0=self.Args['x0'], lbx=self.Args['lbx'], ubx=self.Args['ubx'], 
                           lbg=self.Args['lbg'], ubg=self.Args['ubg'], p=self.Args['p'])
-        self.Controls = np.reshape(sol['x'][3 * (self.N + 1):].full().T, (self.N, 2))
-
+        self.Controls = np.reshape(sol['x'][self.NumStates * (self.N + 1):].full().T, (self.N, 2)) 
         return self
 
-    def _cost_function(self, X, U, P, n_states, n_controls):
+    def _cost_function(self, X, U, P):
         cost = 0
-        for k in range(1, self.N):
-            state = X[:, k]
-            control = U[:, k]
-
-            # Sum of n_states + n_controls
-            n_var = n_states + n_controls
-
-            # Cost function to minimize
-            cost += ca.mtimes([(state - P[n_var * k-2:n_var * k + 1]).T, self.Q, (state - P[n_var * k-2:n_var * k + 1])]) + \
-                    ca.mtimes([(control - P[n_var * k + 1:n_var * k + 3]).T, self.R, (control - P[n_var * k + 1:n_var * k + 3])])
-        return cost
-
-    def _nonlinear_constraints(self, f, X, U, P):
-        # Initial condition constraints
-        constraints = X[:, 0] - P[0:3]
         for k in range(self.N):
             state = X[:, k]
             control = U[:, k]
 
-            st_next = X[:, k + 1]
+            # Cost function to minimize
+            cost += ca.mtimes([(state - P[(self.NumStates+self.NumControls)*(k+1)-2 : (self.NumStates+self.NumControls)*(k+1)-2+self.NumStates]).T, self.Q, (state - P[(self.NumStates+self.NumControls)*(k+1)-2 : (self.NumStates+self.NumControls)*(k+1)-2+self.NumStates])]) + \
+                    ca.mtimes([(control - P[(self.NumStates+self.NumControls)*(k+1)+1 : (self.NumStates+self.NumControls)*(k+1)+1+self.NumControls]).T, self.R, (control - P[(self.NumStates+self.NumControls)*(k+1)+1 : (self.NumStates+self.NumControls)*(k+1)+1+self.NumControls])])
+        return cost
+
+    def _nonlinear_constraints(self, f, X, U, P):
+        # Initial condition constraints
+        constraints = X[:, 0] - P[:self.NumStates]
+        for k in range(self.N):
+            state = X[:, k]
+            control = U[:, k]
+
+            st_next = X[:, k+1]
             f_value = f(state, control)
 
             st_next_euler = state + self.sampleTime * f_value
@@ -135,20 +140,20 @@ class MpcOptimizer:
         self.Solver = ca.nlpsol('solver', 'ipopt', nlp_prob, opts)
 
     def _set_args(self):
-        self.Args['lbg'] = np.zeros(3 * (self.N + 1))  # Equality constraints
-        self.Args['ubg'] = np.zeros(3 * (self.N + 1))  # Equality constraints
+        self.Args['lbg'] = ca.DM.zeros(self.NumStates * (self.N + 1), 1)  # Equality constraints
+        self.Args['ubg'] = ca.DM.zeros(self.NumStates * (self.N + 1), 1)  # Equality constraints
 
-        self.Args['lbx'] = np.zeros(3 * (self.N + 1) + 2 * self.N)
-        self.Args['ubx'] = np.zeros(3 * (self.N + 1) + 2 * self.N)
+        self.Args['lbx'] = ca.DM.zeros(self.NumStates * (self.N + 1) + self.NumControls * self.N, 1)
+        self.Args['ubx'] = ca.DM.zeros(self.NumStates * (self.N + 1) + self.NumControls * self.N, 1)
 
-        self.Args['lbx'][0:3 * (self.N + 1):3] = self.PosXLim[0]  # state x lower bound
-        self.Args['ubx'][0:3 * (self.N + 1):3] = self.PosXLim[1]  # state x upper bound
-        self.Args['lbx'][1:3 * (self.N + 1):3] = self.PosYLim[0]  # state y lower bound
-        self.Args['ubx'][1:3 * (self.N + 1):3] = self.PosYLim[1]  # state y upper bound
-        self.Args['lbx'][2:3 * (self.N + 1):3] = self.OrientLim[0]  # state theta lower bound
-        self.Args['ubx'][2:3 * (self.N + 1):3] = self.OrientLim[1]  # state theta upper bound
+        self.Args['lbx'][0:self.NumStates * (self.N + 1):self.NumStates] = self.PosXLim[0]  # state x lower bound
+        self.Args['ubx'][0:self.NumStates * (self.N + 1):self.NumStates] = self.PosXLim[1]  # state x upper bound
+        self.Args['lbx'][1:self.NumStates * (self.N + 1):self.NumStates] = self.PosYLim[0]  # state y lower bound
+        self.Args['ubx'][1:self.NumStates * (self.N + 1):self.NumStates] = self.PosYLim[1]  # state y upper bound
+        self.Args['lbx'][2:self.NumStates * (self.N + 1):self.NumStates] = self.OrientLim[0]  # state theta lower bound
+        self.Args['ubx'][2:self.NumStates * (self.N + 1):self.NumStates] = self.OrientLim[1]  # state theta upper bound
 
-        self.Args['lbx'][3 * (self.N + 1):3 * (self.N + 1) + 2 * self.N:2] = self.VelLim[0]  # v lower bound
-        self.Args['ubx'][3 * (self.N + 1):3 * (self.N + 1) + 2 * self.N:2] = self.VelLim[1]  # v upper bound
-        self.Args['lbx'][3 * (self.N + 1) + 1:3 * (self.N + 1) + 2 * self.N:2] = self.AngVelLim[0]  # omega lower bound
-        self.Args['ubx'][3 * (self.N + 1) + 1:3 * (self.N + 1) + 2 * self.N:2] = self.AngVelLim[1]  # omega upper bound
+        self.Args['lbx'][self.NumStates * (self.N + 1):self.NumStates * (self.N + 1) + 2 * self.N:2] = self.VelLim[0]  # v lower bound
+        self.Args['ubx'][self.NumStates * (self.N + 1):self.NumStates * (self.N + 1) + 2 * self.N:2] = self.VelLim[1]  # v upper bound
+        self.Args['lbx'][self.NumStates * (self.N + 1) + 1:self.NumStates * (self.N + 1) + 2 * self.N:2] = self.AngVelLim[0]  # omega lower bound
+        self.Args['ubx'][self.NumStates * (self.N + 1) + 1:self.NumStates * (self.N + 1) + 2 * self.N:2] = self.AngVelLim[1]  # omega upper bound
